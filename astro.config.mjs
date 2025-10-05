48e49f7b55d6c780cc81723a93acb2023be2cd22
HARD <hardcoretrbg@gmail.com>
2025-10-05 09:05:36 +0000

    chore: add @astrojs/cloudflare adapter and configure SSR for Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
@@ -7,6 +8,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [react(), mdx(), sitemap()],
-  output: "static",
+  integrations: [react(), mdx(), sitemap(), cloudflare()],
+  output: "server",
 });
