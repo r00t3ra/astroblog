595b2ac4bffcb9bd1f434538b1847bdf6fd57745
HARD <hardcoretrbg@gmail.com>
2025-10-05 08:37:19 +0000

    Switch to static output and remove Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,10 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [react(), mdx(), sitemap()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
+  output: "static",
 });
